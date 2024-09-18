485cb214ec306c0f7e0922ee03820c4a562e8c36
herbertdesousa <sousaherbert138@gmail.com>
2024-09-18 10:43:33 -0300

    feat(Person/Repository): impl

---
+++ src/main/kotlin/com/example/person/Repository.kt
@@ -0,0 +1,63 @@
+package com.example.person
+
+import kotlinx.coroutines.Dispatchers
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.javatime.date
+import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
+import org.jetbrains.exposed.sql.transactions.transaction
+import java.time.LocalDate
+
+data class PersonEntity(
+    val name: String,
+    val nickname: String,
+    val birthdate: LocalDate,
+    val stacks: List<String>
+)
+
+class Repository(database: Database) {
+    object People : Table("people") {
+        val id = integer("id").autoIncrement()
+        val name = varchar("name", length = 256)
+        val nickname = varchar("nickname", length = 256).uniqueIndex()
+        val birthdate = date("birthdate")
+        val stacks = varchar("stack", length = 512)
+
+        override val primaryKey = PrimaryKey(id)
+    }
+
+    init {
+        transaction(database) {
+            SchemaUtils.create(People)
+        }
+    }
+
+    private val separator = "|:|"
+
+    private fun serializeStacks(stacks: List<String>): String = stacks.joinToString(separator)
+
+    private fun deserializeStacks(stacks: String): List<String> = stacks.split(separator)
+
+    suspend fun create(payload: PersonEntity): Int = dbQuery {
+        People.insert {
+            it[name] = payload.name
+            it[nickname] = payload.nickname
+            it[birthdate] = payload.birthdate
+            it[stacks] = serializeStacks(payload.stacks)
+        }[People.id]
+    }
+
+    suspend fun findById(id: Int): PersonEntity? {
+        return dbQuery {
+            People.select { People.id eq id }
+                .map { PersonEntity(
+                    it[People.name],
+                    it[People.nickname],
+                    it[People.birthdate],
+                    deserializeStacks(it[People.stacks])
+                ) }
+                .singleOrNull()
+        }
+    }
+
+    private suspend fun <T> dbQuery(block: suspend () -> T): T = newSuspendedTransaction(Dispatchers.IO) { block() }
+}
