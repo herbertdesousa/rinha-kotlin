179dfa722370530d7de6fdbbcc2a771aacc225f2
herbertdesousa <sousaherbert138@gmail.com>
2024-09-18 08:59:23 -0300

    feat(PersonController): base route

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,10 +1,13 @@
 package com.example.plugins
 
+import com.example.person.personRoutes
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
+    personRoutes()
+
     routing {
         get("/") {
             call.respondText("Hello World!")
